c973f6d8c8cc832e7a9ca66b4f89eb510245a84b
F5423325 - James Biddlecombe <james.biddlecombe@fnb.co.za>
2024-08-15 19:37:47 +0200

    List Groups with stateful selectors

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,10 @@
+
 // PascalCasing to capitilise the first letter of each word
 function Message() {
     // JSX: JaveScript XML
+    const name = "James";
+    if (name)
+        return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
 }
 
